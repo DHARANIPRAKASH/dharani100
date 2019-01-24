1bdcee92a5c8f2642e09d3a9dc51cf560bf624d7
DHARANIPRAKASH <42028785+DHARANIPRAKASH@users.noreply.github.com>
2019-01-24 12:21:08 +0530

    Create for.py

---
+++ for.py
@@ -0,0 +1,14 @@
+n=int(input("enter N"))
+k=int(input("enter K"))
+l=[]
+
+for i in range (1,n+1,1):
+    b=int(input("enter values"))
+    l.append(b)
+x=print(l)
+j=0
+for i in range (k):
+    j=j+l[i]
+print(j)
+    
+    
