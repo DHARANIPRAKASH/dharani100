92c10b6367fe22a47703253e672ed1266f7892f1
DHARANIPRAKASH <42028785+DHARANIPRAKASH@users.noreply.github.com>
2019-06-17 11:31:42 +0530

    Update voooo.py

--- voooo.py
+++ voooo.py
@@ -1,7 +1,7 @@
 q=(input("enter character:"))
 if((q=='a')or(q=='e')or(q=='i')or(q=='o')or(q=='u')):
        print("Vowels")
-elif((q==('b')or(q=='c')or(q=='d')or(q=='f')or(q=='g')or(q=='h')or(q=='j')or(q=='k')or(q=='l')or(q=='m')or(q=='n')or(q=='p')or(q=='q')or(q=='r')or(q=='s')or(q=='t')or(q=='v')or(q=='w')or(q='x')or(q=='y')or(q=='z'))):
+elif((q==('b')or(q=='c')or(q=='d')or(q=='f')or(q=='g')or(q=='h')or(q=='j')or(q=='k')or(q=='l')or(q=='m')or(q=='n')or(q=='p')or(q=='q')or(q=='r')or(q=='s')or(q=='t')or(q=='v')or(q=='w')or(q=='x')or(q=='y')or(q=='z'))):
         print("Consonant")
 else:
         print("invalid")
