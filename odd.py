69d18c5853b85591a0cdbc3d8a9a688be234152d
DHARANIPRAKASH <42028785+DHARANIPRAKASH@users.noreply.github.com>
2019-06-17 11:12:09 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,9 +1,9 @@
 a=int(input("enter the number"))
 if(a>0):
     if((a%2)==0):
-        print("even")
+        print("Even")
     else:
-        print("odd")
+        print("Odd")
 else:
     print("invalid")
     
